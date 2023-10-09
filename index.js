3e116496e1b280e475f35cf36e7c7991b64c9e9d
NIkolayMaslakay <96542705+Nikolay0803@users.noreply.github.com>
2023-10-09 14:16:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// import { name } from "./module.js";
+// // import "./style.css";
+// // import isEmail from "validator/lib/isEmail";
+// // import toDate from "validator/lib/toDate";
+
+
+// console.log("Your name: " + name);
+
+
+
+
+// console.log(toDate("12.11.2000"));
+
+// var moment = require("moment");
+
+// console.log(moment().subtract(6, "days").calendar());
+
+
